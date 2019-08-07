2c0020bf19f4946d0978e29ee537cbc9f133d325
Thiago <thiagoocat@gmail.com>
2019-08-07 08:22:44 -0300

    Iniciando o projeto - Criação do backend

---
+++ backend/src/routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const DevController = require("./controllers/DevController");
+const LikeController = require("./controllers/LikeController");
+const DisLikeController = require("./controllers/DisLikeController");
+
+const routes = express.Router();
+
+routes.post("/devs", DevController.store);
+
+routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/dislikes", DisLikeController.store);
+
+routes.get("/devs", DevController.index);
+
+module.exports = routes;
